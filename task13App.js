1c2a3a9fefde3d0327669a16e2edd90e1f355ba0
25mcmc37-aman <25mcmc37@uohyd.ac.in>
2026-03-22 17:55:34 +0530

    Add files via upload

---
+++ task13App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Weather from "./components/Weather";
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h1>🌤 Weather App</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
